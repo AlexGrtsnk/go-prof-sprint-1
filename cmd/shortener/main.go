3677bb3c00ab54f50dce2b822472a0ac86239f21
Alexey Gritsenko <alexey@MacBook-Pro-Alexey.local>
2024-03-19 00:33:43 +0300

    iter1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -14,9 +14,9 @@ import (
 )
 
 type Config struct {
-	Home           string `env:"HOME"`
-	server_address string `env:"SERVER_ADDRESS"`
-	base_url       string `env:"BASE_URL"`
+	Home          string `env:"HOME"`
+	serverAddress string `env:"serverAddress"`
+	baseURL       string `env:"baseURL"`
 }
 
 type transform [1000][2]string
@@ -41,17 +41,17 @@ func mainPage(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			log.Fatal(err)
 		}
-		long_url := string(a)
-		if long_url == "" {
+		longURL := string(a)
+		if longURL == "" {
 			http.Error(w, "Bad data for url shortener", http.StatusBadRequest)
 		}
-		short_url := generateShortKey()
+		shortURL := generateShortKey()
 		b := new(bytes.Buffer)
-		io.WriteString(b, long_url)
-		if short_url != "" {
-			http.Post("http://localhost"+string(flagRunAddr)+vbn+"/"+string(short_url), "text/plain", b)
+		io.WriteString(b, longURL)
+		if shortURL != "" {
+			http.Post("http://localhost"+string(flagRunAddr)+vbn+"/"+string(shortURL), "text/plain", b)
 			w.WriteHeader(http.StatusCreated)
-			io.WriteString(w, "http://localhost"+string(flagRunAddr)+vbn+"/"+short_url)
+			io.WriteString(w, "http://localhost"+string(flagRunAddr)+vbn+"/"+shortURL)
 		} else {
 			http.Error(w, "cant create short url", http.StatusBadRequest)
 		}
@@ -87,11 +87,11 @@ func apiPage(res http.ResponseWriter, req *http.Request) {
 	}
 	if req.Method == http.MethodPost {
 		a, _ := io.ReadAll(req.Body)
-		long_url := string(a)
+		longURL := string(a)
 		vars := mux.Vars(req)
 		id := vars["id"]
 		mas[kol][0] = string(id)
-		mas[kol][1] = string(long_url)
+		mas[kol][1] = string(longURL)
 		kol++
 	}
 }
@@ -112,11 +112,11 @@ func run() error {
 		log.Fatal(err)
 	}
 
-	if cfg.server_address != "" {
-		flagRunAddr = cfg.server_address
+	if cfg.serverAddress != "" {
+		flagRunAddr = cfg.serverAddress
 	}
-	if cfg.base_url != "" {
-		vbn = cfg.base_url
+	if cfg.baseURL != "" {
+		vbn = cfg.baseURL
 	}
 	log.Println(cfg)
 
