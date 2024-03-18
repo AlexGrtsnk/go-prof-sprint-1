10b57b18e86173cc2b49cb04b6e6411d48f88034
Alexey Gritsenko <alexey@MacBook-Pro-Alexey.local>
2024-03-19 01:40:47 +0300

    Whats wrong isnt me, whats wrong is the world!

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,151 @@
 package main
 
-func main() {}
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"math/rand"
+	"net/http"
+
+	"github.com/caarlos0/env/v6"
+	"github.com/gorilla/mux"
+)
+
+type Config struct {
+	Home          string `env:"HOME"`
+	serverAddress string `env:"serverAddress"`
+	baseURL       string `env:"baseURL"`
+}
+
+type transform [1000][2]string
+
+var mas transform
+var kol int
+
+func generateShortKey() string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	const keyLength = 6
+
+	shortKey := make([]byte, keyLength)
+	for i := range shortKey {
+		shortKey[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(shortKey)
+}
+
+func mainPage(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		a, err := io.ReadAll(r.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		longURL := string(a)
+		if longURL == "" {
+			http.Error(w, "Bad data for url shortener", http.StatusBadRequest)
+		}
+		shortURL := generateShortKey()
+		b := new(bytes.Buffer)
+		io.WriteString(b, longURL)
+		if shortURL != "" {
+			resp, err := http.Post(vbn+"/"+string(shortURL), "text/plain", b)
+			if err != nil {
+				return
+			}
+			defer resp.Body.Close()
+			w.WriteHeader(http.StatusCreated)
+			io.WriteString(w, vbn+"/"+shortURL)
+		} else {
+			http.Error(w, "cant create short url", http.StatusBadRequest)
+		}
+		return
+	} else {
+		w.Header().Set("Location", "sadasdsadwwq")
+		w.WriteHeader(http.StatusBadRequest)
+		io.WriteString(w, "No get method allowed")
+	}
+}
+
+func apiPage(res http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodGet {
+		vars := mux.Vars(req)
+		id, ok := vars["id"]
+		if !ok {
+			fmt.Println("id is missing in parameters")
+			res.WriteHeader(http.StatusBadRequest)
+			io.WriteString(res, "bad request")
+		}
+		flag := 0
+		for i := 0; i < 1000; i++ {
+			if mas[i][0] == string(id) {
+				res.Header().Set("Location", mas[i][1])
+				res.WriteHeader(http.StatusTemporaryRedirect)
+				flag = 1
+				break
+			}
+		}
+		//return
+		if flag == 0 {
+			res.WriteHeader(http.StatusNotFound)
+			io.WriteString(res, "No full url for this address")
+		}
+	}
+	if req.Method == http.MethodPost {
+		a, _ := io.ReadAll(req.Body)
+		longURL := string(a)
+		vars := mux.Vars(req)
+		id := vars["id"]
+		mas[kol][0] = string(id)
+		mas[kol][1] = string(longURL)
+		kol++
+	}
+}
+
+func main() {
+	parseFlags()
+
+	if err := run(); err != nil {
+		panic(err)
+	}
+
+}
+
+func run() error {
+	var cfg Config
+	err := env.Parse(&cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if cfg.serverAddress != "" {
+		flagRunAddr = "8080"
+	}
+	if cfg.baseURL != "" {
+		vbn = cfg.baseURL
+	}
+	log.Println(cfg)
+
+	fmt.Println("Running server on", flagRunAddr)
+	fmt.Println("Running api on", vbn)
+	kol = 0
+	mux1 := mux.NewRouter()
+	mux1.HandleFunc(`/{id}`, apiPage)
+	mux1.HandleFunc(`/`, mainPage)
+	return http.ListenAndServe(flagRunAddr, mux1)
+}
+
+var flagRunAddr string
+var vbn string
+
+func parseFlags() {
+	flag.StringVar(&flagRunAddr, "a", "localhost:8080", "address and port to run server")
+	flag.StringVar(&vbn, "b", "http://localhost:8080", "api page existance url adress")
+	flag.Parse()
+	if flagRunAddr != "localhost:8080" && vbn == "http://localhost:8080" {
+		vbn = "http://" + flagRunAddr
+	}
+	if flagRunAddr == "localhost:8080" && vbn != "http://localhost:8080" {
+		flagRunAddr = vbn[7:]
+	}
+}
---
+++ go.mod
@@ -0,0 +1,17 @@
+module go-prof-sprint-1
+
+go 1.21.3
+
+require (
+	github.com/caarlos0/env/v6 v6.10.1
+	github.com/go-resty/resty/v2 v2.12.0
+	github.com/gorilla/mux v1.8.1
+	github.com/stretchr/testify v1.9.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/net v0.22.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
