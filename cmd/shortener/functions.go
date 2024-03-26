021ed7bcd702fa613a36011bc20334d49dc9794b
Alexey Gritsenko <alexey@MacBook-Pro-Alexey.local>
2024-03-27 01:55:33 +0300

    sprint 1 complete

--- cmd/shortener/functions.go
+++ cmd/shortener/functions.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"database/sql"
 	"flag"
 	"fmt"
 	"io"
@@ -33,31 +32,13 @@ func generateShortKey() string {
 }
 
 func mainPage(w http.ResponseWriter, r *http.Request) {
-	var db *sql.DB
-	var vbn string
-	db, err := sql.Open("sqlite3", "conf.db")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-	quer := "SELECT vbn FROM cfg WHERE id = 1;"
-	rows, err := db.Query(quer)
+	vbn, err := dbMnp()
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		_, err = io.WriteString(w, "Error on the database side")
 		if err != nil {
 			log.Fatal(err)
 		}
-		return
-	}
-	defer rows.Close()
-	if rows.Err() != nil {
-		log.Fatal(err)
-	}
-	rows.Next()
-	err = rows.Scan(&vbn)
-	if err != nil {
-		log.Fatal(err)
 	}
 	if r.Method == http.MethodPost {
 		a, err := io.ReadAll(r.Body)
@@ -100,57 +81,34 @@ func mainPage(w http.ResponseWriter, r *http.Request) {
 }
 
 func apiPage(res http.ResponseWriter, req *http.Request) {
-	var db *sql.DB
-	db, err := sql.Open("sqlite3", "shortlongurl.db")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
 	if req.Method == http.MethodGet {
 		vars := mux.Vars(req)
 		id, ok := vars["id"]
 		if !ok {
 			fmt.Println("id is missing in parameters")
 			res.WriteHeader(http.StatusBadRequest)
-			_, err = io.WriteString(res, "bad request")
+			_, err := io.WriteString(res, "bad request")
 			if err != nil {
 				log.Fatal(err)
 			}
 		}
-		flag := 0
-		quer := "SELECT longURL FROM short_longURL WHERE short_url = '" + string(id) + "';"
-		rows, err := db.Query(quer)
+		longURL, flag, err := dbAppgGt(id)
 		if err != nil {
 			res.WriteHeader(http.StatusBadRequest)
 			_, err = io.WriteString(res, "Error on the database side")
 			if err != nil {
 				log.Fatal(err)
 			}
-			return
 		}
-		defer rows.Close()
-		if rows.Err() != nil {
-			log.Fatal(err)
-		}
-
-		for rows.Next() {
-
-			var longURL string
-			flag = 1
-			err = rows.Scan(&longURL)
-			res.Header().Set("Location", longURL)
-			res.WriteHeader(http.StatusTemporaryRedirect)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			fmt.Printf("%s\n", longURL)
-		}
-
 		if flag == 0 {
 			res.WriteHeader(http.StatusNotFound)
 			_, err = io.WriteString(res, "No full url for this address")
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			res.Header().Set("Location", longURL)
+			res.WriteHeader(http.StatusTemporaryRedirect)
 			if err != nil {
 				log.Fatal(err)
 			}
@@ -161,8 +119,7 @@ func apiPage(res http.ResponseWriter, req *http.Request) {
 		longURL := string(a)
 		vars := mux.Vars(req)
 		id := vars["id"]
-		quer := "INSERT INTO short_longURL(short_url, longURL) VALUES('" + string(id) + "', '" + string(longURL) + "');"
-		_, err := db.Exec(quer)
+		err := dbAppgPst(id, longURL)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -171,8 +128,6 @@ func apiPage(res http.ResponseWriter, req *http.Request) {
 }
 
 func run() error {
-	var db *sql.DB
-	var db1 *sql.DB
 	flagRunAddr, vbn := parseFlags()
 	var cfg Config
 	err := env.Parse(&cfg)
@@ -187,34 +142,7 @@ func run() error {
 		vbn = cfg.baseURL
 	}
 	log.Println(cfg)
-	db, err = sql.Open("sqlite3", "shortlongurl.db")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer db.Close()
-
-	sts := `
-DROP TABLE IF EXISTS short_longURL;
-CREATE TABLE short_longURL(id INTEGER PRIMARY KEY, short_url TEXT, longURL TEXT);`
-	_, err = db.Exec(sts)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	db1, err = sql.Open("sqlite3", "conf.db")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer db1.Close()
-	sts1 := `
-DROP TABLE IF EXISTS cfg;
-CREATE TABLE cfg (id INTEGER PRIMARY KEY, flagRunAddr TEXT, vbn TEXT);
-INSERT INTO cfg(flagRunAddr, vbn) VALUES ('` + string(flagRunAddr) + `', '` + string(vbn) + `');`
-	_, err = db1.Exec(sts1)
-
+	err = dbMnCf(flagRunAddr, vbn)
 	if err != nil {
 		log.Fatal(err)
 	}
