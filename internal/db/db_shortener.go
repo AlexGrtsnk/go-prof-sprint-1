f1455818690be18b91f3b15be112fc10a8b9c039
Alexey Gritsenko <alexey@MacBook-Pro.local>
2024-04-25 23:22:19 +0300

    test3

--- internal/db/db_shortener.go
+++ internal/db/db_shortener.go
@@ -500,7 +500,7 @@ func DataBaseDeleteURL(longURL string, tknm string) (err error) {
 	if err != nil {
 		return err
 	}
-	quer := "UPDATE short_longURL SET delFlag=1 WHERE short_url = '" + longURL + " and tknm = '" + tknm + "';" // " and tknm = '" + tknm +
+	quer := "UPDATE short_longURL SET delFlag=1 WHERE short_url = '" + longURL + "' and tknm = '" + tknm + "';" // "' and tknm = '" + tknm +
 	fmt.Println("THIS IS ION COROUTINE: ", longURL)
 	_, err = db.Exec(quer)
 	fmt.Println("SOMETHING HAPPENED", quer)
