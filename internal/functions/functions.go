b37d35c9f5c6738a9205c9f1881fe52501b4e262
Alexey Gritsenko <alexey@MacBook-Pro.local>
2024-04-25 21:13:21 +0300

    test2

--- internal/functions/functions.go
+++ internal/functions/functions.go
@@ -647,7 +647,7 @@ func GetConcreteURLSUser(res http.ResponseWriter, req *http.Request) {
 			http.Error(res, err.Error(), http.StatusBadGateway)
 			return
 		}
-		fmt.Println("this is in delete ", buf)
+		fmt.Println("this is in delete ", buf.String())
 		if err = json.Unmarshal(buf.Bytes(), &newProduceItems); err != nil {
 			http.Error(res, err.Error(), http.StatusForbidden)
 			return
