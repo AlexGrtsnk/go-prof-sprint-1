d173bf7b0b5d74c23748a9c2b02c9ee2ada262cb
Alexey Gritsenko <alexey@MacBook-Pro.local>
2024-06-20 16:44:45 +0300

    test

--- internal/functions/functions_test.go
+++ internal/functions/functions_test.go
@@ -1,11 +1,17 @@
 package internal
 
 import (
+	"bytes"
+	"fmt"
+	apcfg "go-prof-sprint-1/internal/app_config"
+	db "go-prof-sprint-1/internal/db"
 	"io"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/caarlos0/env"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -48,6 +54,92 @@ func TestCreateShortURLPage(t *testing.T) {
 		})
 	}
 }
+
+func TestCreateShortURLPage1(t *testing.T) {
+	type want struct {
+		code        int
+		contentType string
+	}
+	tests := []struct {
+		name string
+		want want
+	}{
+		{
+			name: "negative test #1",
+			want: want{
+				code: 201,
+				//response:    `{"status":"ok"}`,
+				//contentType: "text/plain; charset=utf-8",
+			},
+		},
+	}
+	fmt.Println("gfgdrtgdt")
+	var cfg apcfg.Config
+	err := env.Parse(&cfg)
+	flagRunAddr, apiRunAddr, fileName, databaseDSN := apcfg.ParseFlags()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if cfg.ServerAddress != "" {
+		flagRunAddr = "8080"
+	}
+	if cfg.BaseURL != "" {
+		apiRunAddr = cfg.BaseURL
+	}
+	if cfg.FileStoragePath != "" {
+		fileName = cfg.FileStoragePath
+	}
+	if cfg.DatabaseDSN != "" {
+		databaseDSN = cfg.DatabaseDSN
+	}
+	log.Println(cfg)
+	err = db.DataBaseStartConfig(databaseDSN)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if databaseDSN != "localhost" {
+		err = db.DataBasePingHandler()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	err = db.DataBaseCfg(flagRunAddr, apiRunAddr, fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = db.DataBaseInsert(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("where postgres is hosted:", databaseDSN)
+	fmt.Println("where db is held", fileName)
+	fmt.Println("Running server on", flagRunAddr)
+	fmt.Println("Running api on", apiRunAddr)
+	fmt.Println("gfgdrtgdt")
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			b := new(bytes.Buffer)
+			_, err = io.WriteString(b, "dasdsdqwe")
+			request := httptest.NewRequest(http.MethodPost, "http://localhost:8080/dasdsdqwe", b)
+			// создаём новый Recorder
+			w := httptest.NewRecorder()
+			createShortURLPage(w, request)
+
+			res := w.Result()
+			// проверяем код ответа
+			assert.Equal(t, test.want.code, res.StatusCode)
+			// получаем и проверяем тело запроса
+			defer res.Body.Close()
+			_, err := io.ReadAll(res.Body)
+
+			require.NoError(t, err)
+			//assert.JSONEq(t, test.want.response, string(resBody))
+			assert.Equal(t, test.want.contentType, res.Header.Get("Content-Type"))
+		})
+	}
+}
+
 func TestDownloadFullURLPage(t *testing.T) {
 	type want struct {
 		code        int
