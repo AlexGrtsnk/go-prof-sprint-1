843988d27d9456211568ce53b00cd38bf730d806
Alexey Gritsenko <alexey@MacBook-Pro.local>
2024-06-20 15:37:34 +0300

    test

--- internal/gzp/gzp_test.go
+++ internal/gzp/gzp_test.go
@@ -5,10 +5,35 @@ import (
 	"testing"
 )
 
-func TestNewProducer(t *testing.T) {
+func TestGzipFormatHandlerJSON(t *testing.T) {
 	request, _ := http.NewRequest("POST", "/", nil)
 	_, err := GzipFormatHandlerJSON(nil, request)
 	if err != nil {
 		t.Errorf("IntMin(2, -2) = %d; want -2", err)
 	}
+}
+
+func TestGzipWrite(t *testing.T) {
+	//request, _ := http.NewRequest("POST", "/", nil)
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("The code did not panic")
+		}
+	}()
+	var v gzipWriter
+	_, _ = v.Write([]byte("string"))
+	//_, err := GzipFormatHandlerJSON(nil, request)
+
+}
+
+func TestGzipHandle(t *testing.T) {
+	//request, _ := http.NewRequest("POST", "/", nil)
+	defer func() {
+		if r := recover(); r != nil {
+			t.Errorf("The code did panic")
+		}
+	}()
+	GzipHandle(nil)
+	//_, err := GzipFormatHandlerJSON(nil, request)
+
 }
