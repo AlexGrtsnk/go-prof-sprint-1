8a9dd9db4890f16c3d926402652637dc55b7e389
Alexey Gritsenko <alexey@MacBook-Pro.local>
2024-07-04 23:45:05 +0300

    sprint 8 complete

---
+++ cmd/staticlint/staticlint_tеst.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"testing"
+
+	"golang.org/x/tools/go/analysis/analysistest"
+)
+// функция теста
+func TestMyAnalyzer(t *testing.T) {
+	// функция analysistest.Run применяет тестируемый анализатор ErrCheckAnalyzer
+	// к пакетам из папки testdata и проверяет ожидания
+	// ./... — проверка всех поддиректорий в testdata
+	// можно указать ./pkg1 для проверки только pkg1
+	defer func() {
+		if r := recover(); r != nil {
+			t.Errorf("The code did not panic")
+		}
+	}()
+	analysistest.Run(t, ".../internal", ErrCheckAnalyzer, "./...")
+
+}
